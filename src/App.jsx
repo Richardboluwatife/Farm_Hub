cb7abe3d2b803eae890d19ba19eda0521a03eccc
Richardboluwatife <richardfadareboluwatife@gmail.com>
2024-03-13 22:37:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Landing from './Landing'
+import Landing2 from './Landing2'
+import Landing3 from './Landing3'
+const App = () => {
+  return (
+    <div>
+        <Landing/>
+        <Landing2/>
+        <Landing3/>
+    </div>
+  )
+}
+
+export default App
